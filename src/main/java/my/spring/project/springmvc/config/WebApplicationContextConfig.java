d77ca05ff4cfa1e9a43c33b3d1e026ae9d1b3f04
a.veshtard <zenonwch@rambler.ru>
2019-02-10 19:44:38 +0200

    initial commit

---
+++ src/main/java/my/spring/project/springmvc/config/WebApplicationContextConfig.java
@@ -0,0 +1,31 @@
+package my.spring.project.springmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("my.spring.project.springmvc")
+public class WebApplicationContextConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html lang="en">
+	<head>
+		<title>Welcome</title>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="jumbotron">
+			<h1><c:out value="${greeting}"/></h1>
+			<p><c:out value="${tagline}"/></p>
+		</div>
+	</body>
+</html>
