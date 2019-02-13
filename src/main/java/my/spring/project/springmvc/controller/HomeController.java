80f2750ec92165667472a1c4d80abe5a7ef35604
a.veshtard <zenonwch@rambler.ru>
2019-02-13 20:00:39 +0200

    redirect with flash attributes

--- src/main/java/my/spring/project/springmvc/controller/HomeController.java
+++ src/main/java/my/spring/project/springmvc/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Locale;
 
@@ -19,13 +20,22 @@ public class HomeController {
         this.msgSource = msgSource;
     }
 
+    // https://stackoverflow.com/questions/13247239/spring-mvc-controller-redirect-without-parameters-being-added-to-my-url
     @GetMapping
-    public String welcome(final Model model, final Locale locale) {
+    public String welcome(final Model model, final Locale locale,
+                          final RedirectAttributes redirectAttributes) {
         final String greeting = msgSource.getMessage("welcome.page.greeting", null, locale);
         model.addAttribute("greeting", greeting);
+        redirectAttributes.addFlashAttribute("greeting", greeting);
         final String tagline = msgSource.getMessage("welcome.page.tagline", null, locale);
         model.addAttribute("tagline", tagline);
+        redirectAttributes.addFlashAttribute("tagline", tagline);
 
+        return "redirect:/welcome/greeting";
+    }
+
+    @GetMapping("/welcome/greeting")
+    public String greeting() {
         return "welcome";
     }
 }
